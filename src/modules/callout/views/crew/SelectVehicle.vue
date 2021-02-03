ed5c3586fe77aaee6b0b906e57dd28d3b0caa69a
Jonas Gampig <jonas.gampig@tum.de>
2021-02-03 20:31:43 +0100

    refactor(SelectVehicle): remove wrapping card

--- src/modules/callout/views/crew/SelectVehicle.vue
+++ src/modules/callout/views/crew/SelectVehicle.vue
@@ -1,80 +1,66 @@
 <template>
   <CrewPage page-title="Fahrzeug auswählen">
-    <v-card :loading="loadingCallout || loadingVehicles">
-      <v-container fluid>
-        <v-row v-if="sortedVehicles.used.length > 0">
-          <v-col
-            v-for="vehicle in sortedVehicles.used"
-            :key="vehicle.id"
-            cols="6"
-            sm="4"
-            md="3"
-            lg="2"
-          >
-            <VehicleCard
-              :value="vehicle"
-              color="secondary"
-              dark
-              outlined
-              @click="selectVehicle(vehicle.id)"
-            >
-            </VehicleCard>
-          </v-col>
-        </v-row>
+    <v-row v-if="sortedVehicles.used.length > 0">
+      <v-col
+        v-for="vehicle in sortedVehicles.used"
+        :key="vehicle.id"
+        cols="6"
+        sm="4"
+        md="3"
+        lg="2"
+      >
+        <VehicleCard
+          :value="vehicle"
+          color="secondary"
+          dark
+          @click="selectVehicle(vehicle.id)"
+        >
+        </VehicleCard>
+      </v-col>
+    </v-row>
 
-        <v-row>
-          <v-col
-            v-for="vehicle in sortedVehicles.active"
-            :key="vehicle.id"
-            cols="6"
-            sm="4"
-            md="3"
-            lg="2"
-          >
-            <VehicleCard
-              :value="vehicle"
-              outlined
-              @click="selectVehicle(vehicle.id)"
-            >
-            </VehicleCard>
-          </v-col>
-        </v-row>
+    <v-row>
+      <v-col
+        v-for="vehicle in sortedVehicles.active"
+        :key="vehicle.id"
+        cols="6"
+        sm="4"
+        md="3"
+        lg="2"
+      >
+        <VehicleCard :value="vehicle" @click="selectVehicle(vehicle.id)">
+        </VehicleCard>
+      </v-col>
+    </v-row>
 
-        <v-row v-if="sortedVehicles.inactive.length > 0">
-          <v-col cols="12" sm="8" md="6" lg="4">
-            <v-btn
-              depressed
-              block
-              @click="showAllInactiveVehicles = !showAllInactiveVehicles"
-            >
-              Ehemalige Fahrzeuge
-              <v-icon right>{{
-                showAllInactiveVehicles ? "mdi-chevron-up" : "mdi-chevron-down"
-              }}</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-expand-transition>
-          <v-row v-show="showAllInactiveVehicles">
-            <v-col
-              v-for="vehicle in sortedVehicles.inactive"
-              :key="vehicle.id"
-              cols="6"
-              sm="4"
-              md="3"
-              lg="2"
-            >
-              <VehicleCard
-                :value="vehicle"
-                outlined
-                @click="selectVehicle(vehicle.id)"
-              >
-              </VehicleCard>
-            </v-col>
-          </v-row>
-        </v-expand-transition>
-      </v-container>
-    </v-card>
+    <v-row v-if="sortedVehicles.inactive.length > 0">
+      <v-col cols="12">
+        <v-btn
+          block
+          @click="showAllInactiveVehicles = !showAllInactiveVehicles"
+        >
+          Ehemalige Fahrzeuge
+          <v-icon right>{{
+            showAllInactiveVehicles ? "mdi-chevron-up" : "mdi-chevron-down"
+          }}</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-expand-transition>
+      <v-row v-show="showAllInactiveVehicles">
+        <v-col
+          v-for="vehicle in sortedVehicles.inactive"
+          :key="vehicle.id"
+          cols="6"
+          sm="4"
+          md="3"
+          lg="2"
+        >
+          <VehicleCard :value="vehicle" @click="selectVehicle(vehicle.id)">
+          </VehicleCard>
+        </v-col>
+      </v-row>
+    </v-expand-transition>
   </CrewPage>
 </template>
 
