e679a9c760dac0dcd6a870bdec58069af3ae8b80
Jonas Gampig <Jonas.Gampig@web.de>
2021-03-28 19:09:44 +0200

    refactor(callout): SelectCrew table instead of custom list

--- src/modules/callout/views/crew/SelectCrew.vue
+++ src/modules/callout/views/crew/SelectCrew.vue
@@ -22,33 +22,93 @@
           @input="onAdd"
         ></PersonAutocomplete>
 
-        <CrewRolesForm
-          :crew="crew"
-          :loading="savingMap"
-          cards-outlined
-          @input="onUpdate"
-          @delete="onRemove"
-        />
+        <v-data-table
+          :headers="headers"
+          :items="crew"
+          :loading="loading"
+          :sort-by="['role']"
+          :sort-desc="[true]"
+          loading-text="Laden..."
+          class="elevation-1"
+          locale="de-DE"
+          item-key="person"
+          hide-default-footer
+          disable-pagination
+        >
+          <template #[`item.action`]="{ item }">
+            <v-edit-dialog
+              :return-value.sync="item.role"
+              cancel-text="Abbrechen"
+              save-text="Speichern"
+              large
+              persistent
+              @save="onUpdate(item)"
+            >
+              <v-btn icon><v-icon>mdi-pencil</v-icon></v-btn>
+              <template #input>
+                <div class="mt-4 title">Funktion ändern</div>
+                <v-select
+                  v-model="item.role"
+                  :items="roles"
+                  label="Funktion"
+                  clearable
+                />
+              </template>
+            </v-edit-dialog>
+
+            <v-btn icon @click="personToRemove = item.person">
+              <v-icon>mdi-delete-outline</v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
       </v-card-text>
     </v-card>
+
+    <v-dialog :value="personToRemove !== null" persistent max-width="300">
+      <v-card>
+        <v-card-title></v-card-title>
+        <v-card-text>
+          Soll {{ personToRemove }} wirklich von der Mannschaft entfernt werden?
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="cancelRemoval()"> Abbrechen </v-btn>
+          <v-spacer />
+          <v-btn text color="primary" @click="confirmRemoval()">
+            Entfernen
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </CrewPage>
 </template>
 
 <script>
 import { mapState, mapActions, mapGetters } from "vuex";
+import { ALL_CALLOUT_ROLES } from "../../models/Callout";
 import CalloutGroupMixin from "../../mixins/CalloutGroupMixin";
 import CrewPage from "../../components/CrewPage";
-import CrewRolesForm from "../../components/form/CrewRolesForm";
 import PersonAutocomplete from "../../components/form/PersonAutocomplete";
 
 export default {
-  components: { CrewPage, CrewRolesForm, PersonAutocomplete },
+  components: { CrewPage, PersonAutocomplete },
   mixins: [CalloutGroupMixin],
 
   data() {
     return {
+      roles: ALL_CALLOUT_ROLES,
       adding: false,
       savingMap: {},
+      personToRemove: null,
+
+      headers: [
+        { text: "Person", value: "person" },
+        { text: "Funktion", value: "role" },
+        {
+          text: "Aktionen",
+          value: "action",
+          sortable: false,
+        },
+      ],
     };
   },
 
@@ -83,6 +143,15 @@ export default {
       removeCrewMember: "removeCrewMember",
     }),
 
+    cancelRemoval() {
+      this.personToRemove = null;
+    },
+
+    confirmRemoval() {
+      this.remove(this.personToRemove);
+      this.personToRemove = null;
+    },
+
     onAdd(item) {
       this.adding = true;
 
@@ -107,7 +176,7 @@ export default {
       });
     },
 
-    onRemove(personId) {
+    remove(personId) {
       this.$set(this.savingMap, personId, true);
       this.removeCrewMember({
         vehicleId: this.vehicle.id,
