48a8436cd6ce655eec8307df156e190d347c8f47
Jonas Gampig <Jonas.Gampig@web.de>
2022-11-09 12:50:29 +0000

    csvService: Add BOM

--- src/modules/exporter/services/csvService.ts
+++ src/modules/exporter/services/csvService.ts
@@ -1,6 +1,8 @@
 export function saveAsCsv(data: string[][]) {
+  const BOM = "\uFEFF";
   const csvContent =
     "data:text/csv;charset=utf-8," +
+    BOM +
     data.map((row) => row.join(";")).join("\n");
   const encodedContent = encodeURI(csvContent);
   window.open(encodedContent);
