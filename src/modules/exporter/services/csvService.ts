4bd6bea136b18a6828de2244a95ff46a3fe8badc
Jonas Gampig <jonas.gampig@web.de>
2022-11-13 15:10:43 +0000

    Fix saveAsCsv encoding

--- src/modules/exporter/services/csvService.ts
+++ src/modules/exporter/services/csvService.ts
@@ -1,9 +1,14 @@
-export function saveAsCsv(data: string[][]) {
+export function saveAsCsv(data: string[][], fileName: string = "Download.csv") {
   const BOM = "\uFEFF";
   const csvContent =
     "data:text/csv;charset=utf-8," +
     BOM +
-    data.map((row) => row.join(";")).join("\n");
-  const encodedContent = encodeURI(csvContent);
-  window.open(encodedContent);
+    encodeURIComponent(data.map((row) => row.join(";")).join("\n"));
+
+  const link = document.createElement("a");
+  link.setAttribute("href", csvContent);
+  link.setAttribute("download", fileName);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
