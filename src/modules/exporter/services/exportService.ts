e0d7b944eead3eebad4d2552f833881b265ecb8d
Jonas Gampig <jonas.gampig@web.de>
2022-11-26 11:04:44 +0000

    exportMannschaftsbuch: Move mannschaft utils to new file

--- src/modules/exporter/services/exportService.ts
+++ src/modules/exporter/services/exportService.ts
@@ -3,36 +3,7 @@ import { Crew } from "@/modules/callout/models/Callout";
 import { Person } from "@/modules/people/models/Person";
 import { FahrzeugeMap } from "@/modules/vehicles/models/Vehicle";
 import { CalloutFormatter } from "../utils/CalloutFormatter";
-
-function isPersonInMannschaft(person: Person, mannschaft: Crew): boolean {
-  const isInBereitschaft: boolean =
-    mannschaft.standby != undefined && mannschaft.standby[person.id] != null;
-
-  const isInAnyFahrzeug: boolean =
-    mannschaft.vehicles != undefined &&
-    Object.values(mannschaft.vehicles).some(
-      (personen) => personen[person.id] != null
-    );
-
-  return isInBereitschaft || isInAnyFahrzeug;
-}
-
-function getGroupOfPerson(
-  person: Person,
-  mannschaft: Crew,
-  fahrzeuge: FahrzeugeMap
-): string {
-  if (mannschaft.standby && mannschaft.standby[person.id]) {
-    return "Bereitschaft";
-  } else if (mannschaft.vehicles) {
-    for (const fahrzeugId in mannschaft.vehicles) {
-      if (mannschaft.vehicles[fahrzeugId][person.id]) {
-        return fahrzeuge[fahrzeugId]?.name || fahrzeugId;
-      }
-    }
-  }
-  return "";
-}
+import { isPersonInMannschaft, getGroupOfPerson } from "../utils/mannschaft";
 
 /**
  *
