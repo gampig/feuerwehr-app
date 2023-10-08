999f4ddae402700c5a482236eae26707c4d92505
Jonas Gampig <jonas.gampig@web.de>
2023-10-08 21:19:57 +0000

    Migrate errorReport to new interface

--- src/services/errorReport.ts
+++ src/services/errorReport.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import firebase from "firebase/app";
 import deviceCredentials, { deviceId } from "@/services/device";
 import version from "@/utils/version";
+import { ComponentPublicInstance } from "vue";
 
 type Severity = "error" | "warning" | "info";
 
@@ -105,8 +106,8 @@ export default class ErrorReportBuilder {
     return this;
   }
 
-  setVue(vm: Vue): ErrorReportBuilder {
-    this.report.context = vm.$route.fullPath;
+  setVue(instance: ComponentPublicInstance): ErrorReportBuilder {
+    this.report.context = instance.$route.fullPath;
     return this;
   }
 
