ef0a484d2d942a51830541dc919ab4a374b021dc
Jonas Gampig <jonas.gampig@web.de>
2024-10-20 11:02:51 +0000

    Add databaseSchemaVersion check

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="feuerwehr-app">
     <router-view />
-    <Loading :visible="loading" text="Anmelden..." />
+    <Loading :visible="loading" :text="loadingScreenText" />
   </v-app>
 </template>
 
@@ -12,28 +12,50 @@ import Loading from "@/components/Loading";
 import { requires } from "./utils/routerAuth";
 import { mapActions, mapState } from "pinia";
 import { useAuthStore } from "./stores/auth";
+import { useDatabaseSchemaStore } from "./stores/databaseSchema";
 
 export default {
   components: { Loading },
 
   computed: {
-    ...mapState(useAuthStore, ["loading", "loggedIn"]),
+    ...mapState(useAuthStore, { loadingAuth: "loading", loggedIn: "loggedIn" }),
+    ...mapState(useDatabaseSchemaStore, {
+      loadingDatabaseSchemaVersion: "loading",
+      updateIsRequired: "updateIsRequired",
+    }),
+
+    loading() {
+      return this.loadingAuth || this.loadingDatabaseSchemaVersion;
+    },
+
+    loadingScreenText() {
+      if (this.loadingAuth) {
+        return "Anmelden...";
+      } else {
+        return "Lade Daten...";
+      }
+    },
   },
 
   watch: {
     loggedIn() {
-      this.onStateChanged();
+      this.onAuthStateChanged();
+    },
+
+    updateIsRequired() {
+      this.checkDatabaseSchemaVersion();
     },
   },
 
   created() {
-    this.onStateChanged();
+    this.fetchDatabaseSchemaVersion();
+    this.onAuthStateChanged();
   },
 
   methods: {
     ...mapActions(useAuthStore, ["updateClientMetadata"]),
 
-    onStateChanged() {
+    onAuthStateChanged() {
       if (this.loggedIn === true) {
         this.onLogin();
       } else if (this.loggedIn === false) {
@@ -62,6 +84,19 @@ export default {
           name: "UserLogin",
           params: { nextUrl: { name: "Home" } },
         });
+      }
+    },
+
+    fetchDatabaseSchemaVersion() {
+      useDatabaseSchemaStore().bind();
+    },
+
+    checkDatabaseSchemaVersion() {
+      const store = useDatabaseSchemaStore();
+      if (store.updateIsRequired) {
+        alert(
+          `Update erforderlich.\nVersion von Database: ${store.remoteSchemaVersion}\nMaximal unterstützte Version von FeuerwehrApp: ${store.localSchemaVersion}`
+        );
       }
     },
   },
