16764b2871f3f4f30c7f81df4a8d8e18dc823e84
Jonas Gampig <jonas.gampig@web.de>
2024-12-21 15:18:01 +0000

    vite.config: Define GIT_HASH and BUILD_TIME

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import path from "path";
+import { gitDescribeSync } from "git-describe";
+
+const buildTimestamp = new Date().toISOString();
+const gitCommitHash = gitDescribeSync().hash;
 
 export default defineConfig({
+  define: {
+    BUILD_TIME: JSON.stringify(buildTimestamp),
+    GIT_HASH: JSON.stringify(gitCommitHash),
+  },
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
