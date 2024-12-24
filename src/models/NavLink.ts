5c6f79aaaf996215299bb255604e88ec7a02d5eb
Jonas Gampig <jonas.gampig@web.de>
2024-12-24 09:53:02 +0000

    Fix NavLink.to type

--- src/models/NavLink.ts
+++ src/models/NavLink.ts
@@ -1,10 +1,10 @@
-import { Location } from "vue-router";
+import { RouteLocationAsPath, RouteLocationAsRelative } from "vue-router";
 
 export interface NavLink {
   title: string;
   icon?: string;
 
-  to?: string | Location;
+  to?: string | RouteLocationAsRelative | RouteLocationAsPath;
   href?: string;
   click?: (event: MouseEvent | KeyboardEvent) => void;
 
