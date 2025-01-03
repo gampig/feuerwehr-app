fe4ce6dd31cb345e773b6650e3a4afef3436b3c4
Jonas Gampig <jonas.gampig@web.de>
2025-01-03 08:23:11 +0000

    clothes: Remove unnecessary import defineEmits

--- src/modules/clothes/components/orders/OrderCard.vue
+++ src/modules/clothes/components/orders/OrderCard.vue
@@ -74,7 +74,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineEmits } from "vue";
+import { computed } from "vue";
 
 const formatter = new Intl.NumberFormat("de-DE", {
   minimumFractionDigits: 0,
