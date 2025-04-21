7ddf714aa97fd3139e020c750f872653f5bf29e2
Jonas Gampig <jonas.gampig@web.de>
2025-04-21 14:25:21 +0000

    callout: Add more info to ReportDialog

--- src/modules/callout/components/ReportDialog.vue
+++ src/modules/callout/components/ReportDialog.vue
@@ -11,15 +11,13 @@
         <v-toolbar-title>Einsatzbericht</v-toolbar-title>
       </v-toolbar>
 
-      <v-container>
+      <v-container fluid>
         <v-form v-if="calloutStore.callout">
           <v-row>
             <v-col cols="6">
               <v-text-field
                 label="Einsatzbeginn"
                 :model-value="formatDateTime(calloutStore.callout.alarmTime)"
-                disabled
-                hide-details
               />
             </v-col>
             <v-col cols="6">
@@ -36,16 +34,12 @@
               <v-text-field
                 label="Stichwort"
                 :model-value="calloutStore.callout.keyword"
-                disabled
-                hide-details
               />
             </v-col>
             <v-col cols="6">
               <v-text-field
                 label="Schlagwort"
                 :model-value="calloutStore.callout.catchphrase"
-                disabled
-                hide-details
               />
             </v-col>
           </v-row>
@@ -54,41 +48,124 @@
               <v-text-field
                 label="Einsatzort"
                 :model-value="calloutStore.callout.address"
-                disabled
-                hide-details
               />
             </v-col>
           </v-row>
 
           <v-row>
-            <template
-              v-for="(
-                vehicleWithCrew, vehicleId
-              ) in calloutStore.vehiclesWithCrew"
-              :key="vehicleId"
-            >
-              <v-col>
-                <v-card>
-                  <v-card-title>
-                    {{ vehicleWithCrew.vehicle.name }}
-                  </v-card-title>
-                  <v-list>
-                    <v-list-item v-if="vehicleWithCrew.calloutDetails">
-                      Einsatzende:
-                      {{
-                        formatDateTime(
-                          Number(vehicleWithCrew.calloutDetails?.endTime)
-                        )
-                      }}
-                    </v-list-item>
-                    <v-list-item>
-                      Anzahl Personal:
-                      {{ Object.keys(vehicleWithCrew.crewMembers).length }}
-                    </v-list-item>
-                  </v-list>
-                </v-card>
-              </v-col>
-            </template>
+            <v-col>
+              <v-expansion-panels
+                multiple
+                :model-value="Object.keys(calloutStore.vehiclesWithCrew)"
+              >
+                <v-expansion-panel
+                  v-for="vehicle in vehiclesStore.vehicles"
+                  :key="vehicle.id"
+                  :value="vehicle.id"
+                  :title="vehicle.name"
+                  :color="
+                    vehicle.id in calloutStore.vehiclesWithCrew
+                      ? 'success'
+                      : undefined
+                  "
+                >
+                  <v-expansion-panel-text>
+                    <v-text-field
+                      label="Einsatzende"
+                      :model-value="
+                        calloutStore.vehiclesWithCrew[vehicle.id]
+                          ?.calloutDetails
+                          ? formatDateTime(
+                              Number(
+                                calloutStore.vehiclesWithCrew[vehicle.id]
+                                  .calloutDetails?.endTime
+                              )
+                            )
+                          : undefined
+                      "
+                    />
+                    <v-text-field label="Gefahrene Kilometer" type="number" />
+                    <v-expansion-panels>
+                      <v-expansion-panel>
+                        <v-expansion-panel-title>
+                          Anzahl Personal:
+                          {{
+                            calloutStore.vehiclesWithCrew[vehicle.id]
+                              ?.crewMembers
+                              ? Object.keys(
+                                  calloutStore.vehiclesWithCrew[vehicle.id]
+                                    .crewMembers
+                                ).length
+                              : 0
+                          }}
+                        </v-expansion-panel-title>
+                        <v-expansion-panel-text>
+                          <v-list>
+                            <v-list-item
+                              v-for="crewMember in calloutStore
+                                .vehiclesWithCrew[vehicle.id]?.crewMembers
+                                ? Object.keys(
+                                    calloutStore.vehiclesWithCrew[vehicle.id]
+                                      .crewMembers
+                                  )
+                                : []"
+                              :key="crewMember"
+                            >
+                              {{ crewMember }}
+                            </v-list-item>
+                          </v-list>
+                        </v-expansion-panel-text>
+                      </v-expansion-panel>
+                    </v-expansion-panels>
+                  </v-expansion-panel-text>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-textarea
+                label="Eingesetzte Gerätschaften"
+                rows="1"
+                auto-grow
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-textarea
+                label="Schäden bzw. sonstige Informationen"
+                rows="1"
+                auto-grow
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-textarea label="Kurzer Einsatzbericht" />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-textarea
+                label="Verursacher (falls bekannt)"
+                rows="1"
+                auto-grow
+              />
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-text-field label="Name Einheitsführer" />
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-btn color="primary">Abschließen & Versenden</v-btn>
+            </v-col>
           </v-row>
         </v-form>
       </v-container>
@@ -100,6 +177,7 @@
 import { formatDateTime } from "@/utils/dates";
 import { useCalloutStore } from "../stores/callout";
 import { computed } from "vue";
+import { useVehiclesStore } from "@/modules/vehicles/stores/vehicles";
 
 defineProps({
   modelValue: {
@@ -110,6 +188,7 @@ defineProps({
 const emit = defineEmits(["update:model-value"]);
 
 const calloutStore = useCalloutStore();
+const vehiclesStore = useVehiclesStore();
 
 const endTime = computed(() => {
   if (calloutStore.callout?.vehicles) {
