0ea0fecfa8a414511324aa1aed6030f0ae5e7fa2
Jonas Gampig <jonas.gampig@web.de>
2025-04-21 18:43:15 +0000

    callout: Change layout of ReportForm

--- src/modules/callout/components/form/ReportForm.vue
+++ src/modules/callout/components/form/ReportForm.vue
@@ -1,151 +1,111 @@
 <template>
-  <v-container fluid>
+  <v-container>
     <v-form v-if="calloutStore.callout">
-      <v-row>
-        <v-col cols="6">
-          <v-text-field
-            label="Einsatzbeginn"
-            :model-value="formatDateTime(calloutStore.callout.alarmTime)"
-          />
-        </v-col>
-        <v-col cols="6">
-          <v-text-field
-            label="Einsatzende"
-            :model-value="formatDateTime(endTime)"
-            disabled
-            hide-details
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="6">
-          <v-text-field
-            label="Stichwort"
-            :model-value="calloutStore.callout.keyword"
-          />
-        </v-col>
-        <v-col cols="6">
-          <v-text-field
-            label="Schlagwort"
-            :model-value="calloutStore.callout.catchphrase"
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-text-field
-            label="Einsatzort"
-            :model-value="calloutStore.callout.address"
-          />
-        </v-col>
-      </v-row>
+      <v-text-field
+        label="Einsatzbeginn"
+        :model-value="formatDateTime(calloutStore.callout.alarmTime)"
+      />
+      <v-text-field
+        label="Einsatzende"
+        :model-value="formatDateTime(endTime)"
+        disabled
+      />
+      <v-text-field
+        label="Stichwort"
+        :model-value="calloutStore.callout.keyword"
+      />
+      <v-text-field
+        label="Schlagwort"
+        :model-value="calloutStore.callout.catchphrase"
+      />
+      <v-text-field
+        label="Einsatzort"
+        :model-value="calloutStore.callout.address"
+      />
 
-      <v-row>
-        <v-col>
-          <v-expansion-panels
-            multiple
-            :model-value="Object.keys(calloutStore.vehiclesWithCrew)"
-          >
-            <v-expansion-panel
-              v-for="vehicle in vehiclesStore.vehicles"
-              :key="vehicle.id"
-              :value="vehicle.id"
-              :title="vehicle.name"
-              :color="
-                vehicle.id in calloutStore.vehiclesWithCrew
-                  ? 'success'
+      <v-text-field label="Name Einheitsführer" />
+
+      <v-textarea label="Verursacher (falls bekannt)" auto-grow />
+
+      <v-textarea label="Kurzer Einsatzbericht (optional)" auto-grow />
+
+      <h2>Einsatzmittel</h2>
+      <v-expansion-panels
+        multiple
+        :model-value="Object.keys(calloutStore.vehiclesWithCrew)"
+      >
+        <v-expansion-panel
+          v-for="vehicle in vehiclesStore.vehicles"
+          :key="vehicle.id"
+          :value="vehicle.id"
+          :title="vehicle.name"
+          :color="
+            vehicle.id in calloutStore.vehiclesWithCrew
+              ? 'secondary'
+              : undefined
+          "
+        >
+          <v-expansion-panel-text>
+            <v-text-field
+              label="Einsatzende"
+              :model-value="
+                calloutStore.vehiclesWithCrew[vehicle.id]?.calloutDetails
+                  ? formatDateTime(
+                      Number(
+                        calloutStore.vehiclesWithCrew[vehicle.id].calloutDetails
+                          ?.endTime
+                      )
+                    )
                   : undefined
               "
-            >
-              <v-expansion-panel-text>
-                <v-text-field
-                  label="Einsatzende"
-                  :model-value="
-                    calloutStore.vehiclesWithCrew[vehicle.id]?.calloutDetails
-                      ? formatDateTime(
-                          Number(
+            />
+            <v-text-field label="Gefahrene Kilometer" type="number" />
+            <v-expansion-panels>
+              <v-expansion-panel>
+                <v-expansion-panel-title>
+                  Anzahl Personal:
+                  {{
+                    calloutStore.vehiclesWithCrew[vehicle.id]?.crewMembers
+                      ? Object.keys(
+                          calloutStore.vehiclesWithCrew[vehicle.id].crewMembers
+                        ).length
+                      : 0
+                  }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <v-list>
+                    <v-list-item
+                      v-for="crewMember in calloutStore.vehiclesWithCrew[
+                        vehicle.id
+                      ]?.crewMembers
+                        ? Object.keys(
                             calloutStore.vehiclesWithCrew[vehicle.id]
-                              .calloutDetails?.endTime
+                              .crewMembers
                           )
-                        )
-                      : undefined
-                  "
-                />
-                <v-text-field label="Gefahrene Kilometer" type="number" />
-                <v-expansion-panels>
-                  <v-expansion-panel>
-                    <v-expansion-panel-title>
-                      Anzahl Personal:
-                      {{
-                        calloutStore.vehiclesWithCrew[vehicle.id]?.crewMembers
-                          ? Object.keys(
-                              calloutStore.vehiclesWithCrew[vehicle.id]
-                                .crewMembers
-                            ).length
-                          : 0
-                      }}
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text>
-                      <v-list>
-                        <v-list-item
-                          v-for="crewMember in calloutStore.vehiclesWithCrew[
-                            vehicle.id
-                          ]?.crewMembers
-                            ? Object.keys(
-                                calloutStore.vehiclesWithCrew[vehicle.id]
-                                  .crewMembers
-                              )
-                            : []"
-                          :key="crewMember"
-                        >
-                          {{ crewMember }}
-                        </v-list-item>
-                      </v-list>
-                    </v-expansion-panel-text>
-                  </v-expansion-panel>
-                </v-expansion-panels>
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-col>
-      </v-row>
+                        : []"
+                      :key="crewMember"
+                    >
+                      {{ crewMember }}
+                    </v-list-item>
+                  </v-list>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
 
-      <v-row>
-        <v-col>
-          <v-textarea label="Eingesetzte Gerätschaften" rows="1" auto-grow />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-textarea
-            label="Schäden bzw. sonstige Informationen"
-            rows="1"
-            auto-grow
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-textarea label="Kurzer Einsatzbericht" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-textarea label="Verursacher (falls bekannt)" rows="1" auto-grow />
-        </v-col>
-      </v-row>
+      <h2>Gerätschaften</h2>
+      <v-textarea label="Eingesetzte Gerätschaften (optional)" auto-grow />
 
-      <v-row>
-        <v-col>
-          <v-text-field label="Name Einheitsführer" />
-        </v-col>
-      </v-row>
+      <h2>Weitere Informationen</h2>
+      <v-textarea
+        label="Schäden bzw. sonstige Informationen (optional)"
+        auto-grow
+      />
 
-      <v-row>
-        <v-col>
-          <v-btn color="primary">Abschließen & Versenden</v-btn>
-        </v-col>
-      </v-row>
+      <v-btn class="mr-3">Speichern</v-btn>
+      <v-btn color="primary">Speichern & Versenden</v-btn>
     </v-form>
   </v-container>
 </template>
